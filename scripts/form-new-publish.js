8837b0e80d3ce552caaad10820037edf8ab33064
calis7 <camilotero89@gmail.com>
2023-11-04 14:02:56 +0100

    Se corrigen las rutas de estilos css

--- scripts/form-new-publish.js
+++ scripts/form-new-publish.js
@@ -120,8 +120,8 @@ document.addEventListener("DOMContentLoaded", function () {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="/styles/post-detail.css">
-                <link rel="stylesheet" href="/styles/style.css">      
+                <link rel="stylesheet" href="styles/post-detail.css">
+                <link rel="stylesheet" href="styles/style.css">      
                 <title>${title} - Detalle de Publicación</title>
             </head>
                         
